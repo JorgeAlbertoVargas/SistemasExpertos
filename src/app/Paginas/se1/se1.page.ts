e8202712ebb988c16b584fa45f807243e52c3eb1
Jvargas <jvargas@syteccorp.com>
2022-03-22 02:27:37 -0600

    Proyecto de Sistemas Expertos

---
+++ src/app/Paginas/se1/se1.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-se1',
+  templateUrl: './se1.page.html',
+  styleUrls: ['./se1.page.scss'],
+})
+export class SE1Page implements OnInit {
+  
+  constructor(private actionSheetCtrl:ActionSheetController  ) { }
+
+  ngOnInit() {
+  }
+
+
+  //Funcion del Boton
+  onClick(){
+    this.presentActionSheet();
+  }
+
+
+  // Crear Fuciones
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        data: 'Data value',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
+}
